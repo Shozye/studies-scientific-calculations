7f09149c2162d0e6eabcd85757cb1ad2373b93e7
Mateusz <sshozye@gmail.com>
2022-11-26 16:03:45 +0100

    fixed ex5

--- assignment_list_3/src/exercise_5.jl
+++ assignment_list_3/src/exercise_5.jl
@@ -7,7 +7,7 @@ include("plot_helpers.jl")
 f(x) = exp(x) - 3x
 
 maxit = 100000
-accuracy = Float64(10^(-5))
+accuracy = Float64(10^(-4))
 
 tests = (
     (Roots.bisection, (f, Float64(0), Float64(1), accuracy, accuracy)),
@@ -41,14 +41,14 @@ end
 root1= 0.6190612867359451121523269940209222333014717772629693524598360744929373522550887346110469261882588406588475092970521772616391443
 root2= 1.5121345516578424738967396780720387046036503851353594542592854739989771605115748273242654881527798352683957198107193203478259518
 
-xs = LinRange(0.619045, 0.619075, 100)
+xs = LinRange(0.6189, 0.6192, 100)
 ys = [f(x) for x in xs]
 p = plot(xs, ys, label="e^x - 3x")
 draw_point_with_accuracy(p, root1, 0, accuracy, accuracy)
 scatter!(p, [results[1][1]], [results[1][2]], color=:green, markersize=7, label="Found root")
 savefig("results/ex5_plot_1.png")
 
-xs = LinRange(1.51212, 1.51215, 100)
+xs = LinRange(1.512, 1.51228, 100)
 ys = [f(x) for x in xs]
 p = plot(xs, ys, label="e^x - 3x")
 draw_point_with_accuracy(p, root2, 0, accuracy, accuracy)
