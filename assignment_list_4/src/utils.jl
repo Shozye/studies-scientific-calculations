70730206c726bf78771303b6de0991416b049daa
Mateusz <sshozye@gmail.com>
2022-12-10 21:54:06 +0100

    added list 4

---
+++ assignment_list_4/src/utils.jl
@@ -0,0 +1,10 @@
+include("../src/interpolation.jl")
+
+using .Interpolation
+using Plots
+
+function plotInterpolations(f, a, b, ns, filename)
+    plots = [drawInterpolation(f, a, b, n) for n in ns]
+    p = plot(plots..., layout=(1, length(ns)), size=(1200, 400))
+    savefig(filename)
+end
